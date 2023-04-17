f309fc1c74523471ca7b2ee0204460327c701a28
Samuel Maltais <leumasmontreal@hotmail.com>
2023-04-17 16:18:32 -0400

    Test script multithreading

---
+++ src/test/java/server/multithreadTest.java
@@ -0,0 +1,10 @@
+package server;
+
+import client.JavaFxGui.Client;
+
+public class multithreadTest {
+    public void main(String args[]) {
+        Client client = new Client();
+        Client client2 = new Client();
+    }
+}
